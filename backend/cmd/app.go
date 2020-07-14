48ca10db0ba603c02d9cf7652bede4d79326ee4f
gamz <gamzabaw@gmail.com>
2020-07-15 00:01:30 +0900

    Delete hello files

--- backend/cmd/app.go
+++ backend/cmd/app.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"oraksil.com/sil/internal/presenter/di"
 	"oraksil.com/sil/internal/presenter/web"
 )
 
 func main() {
-	fmt.Print("Hello World")
-
 	di.InitContainer()
 
 	w := web.NewWebService()
