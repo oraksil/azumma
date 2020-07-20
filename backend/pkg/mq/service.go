0a0c949070e1444e8f48a78d3d27ea93f8261b70
gamz <gamzabaw@gmail.com>
2020-07-21 08:32:59 +0900

    Refactor to extract util funcs

--- backend/pkg/mq/service.go
+++ backend/pkg/mq/service.go
@@ -1,13 +1,11 @@
 package mq
 
 import (
-	"encoding/json"
 	"fmt"
 	"sync"
 	"time"
 
 	"github.com/mitchellh/mapstructure"
-	uuid "github.com/satori/go.uuid"
 	"github.com/segmentio/ksuid"
 	"github.com/streadway/amqp"
 )
@@ -249,45 +247,6 @@ func (mq *MqService) reply(exchange string, origMsg Message, payload interface{}
 		false, // immediate
 		composeMessage(origMsg.MessageId, "", "", payload),
 	)
-}
-
-func composeMessage(msgId, msgType, replyTo string, payload interface{}) amqp.Publishing {
-	if msgId == "" {
-		msgId = uuid.NewV4().String()
-	} else { // assume it's for reply
-		msgType = MSG_TYPE_RESERVED_REPLY
-	}
-
-	body, _ := json.Marshal(payload)
-
-	msg := amqp.Publishing{
-		MessageId:   msgId,
-		ContentType: "application/json",
-		Type:        msgType,
-		Body:        body,
-	}
-
-	if replyTo != "" {
-		msg.ReplyTo = replyTo
-	}
-
-	return msg
-}
-
-func receiveMessageWithTimeout(ch chan Message, timeoutInSecs time.Duration) Message {
-	timeout := make(chan bool, 1)
-	go func() {
-		time.Sleep(timeoutInSecs * time.Second)
-		timeout <- true
-	}()
-
-	var recvMsg Message
-	select {
-	case recvMsg = <-ch:
-	case <-timeout:
-	}
-
-	return recvMsg
 }
 
 func (mq *MqService) AddHandler(handler MessageHandler) {
