77dcf4184b0a587442c37b53795cb85d8e14120c
gamz <gamzabaw@gmail.com>
2020-11-02 23:14:24 +0900

    Add env sample

--- migrations/002_insert_games.up.sql
+++ migrations/002_insert_games.up.sql
@@ -3,7 +3,7 @@ VALUES
     (1, 'Tekken 3', 'NAMCO', 'Tekken 3 (鉄拳3) is a fighting game, the third installment in the Tekken series.', 2, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/packs/tekken3/tekken3_poster.jpg', 'tekken3'),
     (1, 'Bobl Bubl', 'TAITO', '', 2, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/packs/bublbobl/bublbobl_poster.jpg', 'bublbobl'),
     (1, 'Cadilacs Dinosours', 'Capcom', "It is a side-scrolling beat 'em up based on the comic book series Xenozoic Tales. The game was produced as a tie-in to the short-lived Cadillacs and Dinosaurs animated series which was aired during the same year the game was released.", 3, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/packs/dino/dino_poster.jpg', 'dino'),
-    (1, 'Final Fight II', 'Capcom', '', 3, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/packs/ffight2b/ffight2b_poster.png', 'ffight2b'),
+    (0, 'Final Fight II', 'Capcom', '', 3, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/packs/ffight2b/ffight2b_poster.png', 'ffight2b'),
     (1, 'Super Tank', 'Video Games GmbH', '', 2, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/packs/supertnk/supertnk_poster.png', 'supertnk');
 
 INSERT INTO player (name, total_coins)
